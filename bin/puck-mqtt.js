4e5d611991c65b88a072223ea9b3f4f82fafc1c0
Ben Foxall <benfoxall@gmail.com>
2017-02-26 18:26:08 +0000

    Add debugging messages

--- bin/puck-mqtt.js
+++ bin/puck-mqtt.js
@@ -4,16 +4,21 @@ const program = require('commander')
 const version = require('../package.json').version
 const chalk   = require('chalk')
 
-const pmqt = require('../')
-
 const gather = (val, set) => set.add(val)
 
 program
   .version(version)
   .option('-h, --host <hostname>', 'The MQTT host to connect to')
   .option('-p, --puck [puck]', 'A particular puck. eg f045', gather, new Set)
+  .option('-d, --debug')
   .parse(process.argv)
 
+if(program.debug) {
+  process.env.DEBUG = 'puck-mqtt:*'
+}
+
+
+const pmqt = require('../')
 
 const host = program.host || 'mqtt://test.mosquitto.org'
 const puck = program.puck
